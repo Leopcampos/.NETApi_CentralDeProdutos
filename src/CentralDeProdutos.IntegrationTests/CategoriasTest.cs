7cbe94164cc62201ef50aec8eb20761ca24f30f7
Leo Campos <leopcampos85@gmail.com>
2023-06-19 12:04:06 -0300

    First Commit

---
+++ src/CentralDeProdutos.IntegrationTests/CategoriasTest.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+namespace CentralDeProdutos.IntegrationTests
+{
+    public class CategoriasTest
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ src/CentralDeProdutos.IntegrationTests/ProdutosTest.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace CentralDeProdutos.IntegrationTests
+{
+    public class ProdutosTest
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
